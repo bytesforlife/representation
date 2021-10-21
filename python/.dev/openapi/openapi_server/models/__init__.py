e0c9ef0abd96827096df5c45d9ee257e3fc857ce
Hendrik Ballhausen <44930015+Ballhausen@users.noreply.github.com>
2021-10-22 01:04:35 +0200

    remove temp files

--- python/.dev/openapi/openapi_server/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-# flake8: noqa
-from __future__ import absolute_import
-# import models into model package
-from openapi_server.models.args_kwargs import ArgsKwargs
-from openapi_server.models.inline_response_default import InlineResponseDefault
